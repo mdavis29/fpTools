59d595e8727ecb4812aed415678333364ec5914d
mdavis29 <mdavis29@gmail.com>
2017-02-14 07:58:56 -0500

    update2-14-16

--- R/plotTsDensities.R
+++ R/plotTsDensities.R
@@ -1,6 +1,7 @@
 #' @title plot time series density before and after
 #' @param myTs a univariate time series object
 #' @param timeCut numeric date date to cut test before and after ie 2016.5
+#' @param plotName name of the plot to be put in the title
 #' @return density plot of the data before and after the cut off
 #' @author Matthew Davis
 #' @details This creates a ts matrix of lag columns from a ts object 
@@ -13,8 +14,8 @@ plotTsDenisities<-function(myTs, timeCut = c(2016), plotName =NULL ){
                   paste('Before and After', timeCut)), 
      col = 'blue',
      sub = plotName,
-     cex.main = .8,
-     xaxt='n')
+     cex.main = .8)
+
   abline(v = mean(before)+2*sd(before))
   abline(v = mean(before)-2*sd(before))
   abline(v = mean(before), col = 'blue')
